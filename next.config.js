c0ef1539e29b0b67af54f854f9d1524b7e5f4eac
abdoachhoubi <aachhoub@student.1337.ma>
2023-06-04 20:59:05 +0100

    Modified next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  devIndicators: {
-    autoPrerender: false,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
